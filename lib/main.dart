af67f377471ae57da2b851a12093742676684c92
guteco <guteco@gmail.com>
2022-09-08 18:56:45 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:exemplo/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar'),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: Container(
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.red,
+        ),
+        child: Center(
+          child: Text(
+            'Olá Mundo!!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.access_time_sharp),
+          label: 'Item1',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.abc_rounded),
+          label: 'Item2',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.access_alarm_outlined),
+          label: 'Item3',
+        ),
+      ]),
+    );
+  }
+}
